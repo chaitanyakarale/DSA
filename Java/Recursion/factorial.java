6a43cc2b594e4d04e61b118acb330a776d861340
chaitanyakarale <129312459+chaitanyakarale@users.noreply.github.com>
2023-12-22 15:18:35 +0530

    Add files via upload

---
+++ Java/Recursion/factorial.java
@@ -0,0 +1,17 @@
+package Recusion;
+
+public class pfact {
+    public static int fact(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(fact(6));
+
+    }
+}
