afddcad39b81d10785db11058773d8086e08f20c
chaitanyakarale <129312459+chaitanyakarale@users.noreply.github.com>
2023-12-22 15:23:07 +0530

    Create tilling.java

---
+++ Java/Recursion/tilling.java
@@ -0,0 +1,20 @@
+
+public class tiling {
+
+    public static int tiling(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int fnm1 = tiling(n - 1);
+        int fnm2 = tiling(n - 2);
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(tiling(6));
+    }
+
+}
